68866a001251810435bfea03d505c26f0bd703e0
Tim Guite <tim.guite@diamond.ac.uk>
2020-02-06 11:58:01 +0000

    Provided simple builder support for ether_ip

---
+++ etc/builder.py
@@ -0,0 +1,25 @@
+from iocbuilder import AutoSubstitution, Substitution, ModuleBase
+from iocbuilder import Device
+from iocbuilder.arginfo import *
+
+class EtherIPInit(Device):
+    """This creates an IP connection to a plc using the ether_ip driver"""
+    # Dependencies = ()
+    
+    def __init__(self, name, ip):
+        self.__super.__init__()
+        self.name = name
+        self.ip = ip
+
+    def InitialiseOnce(self):
+        print "# EtherIP Initialisation"
+        print "drvEtherIP_init()"
+
+
+    def Initialise(self):
+        print "# Define EtherIP to PLC connection"
+        print "drvEtherIP_define_PLC(\"{0}\", \"{1}\", 0)".format(self.name, self.ip)
+        
+    ArgInfo = makeArgInfo(__init__,    
+        name = Simple("Port Name"),
+        ip = Simple("IP Port of PLC"))
