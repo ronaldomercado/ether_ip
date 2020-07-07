a4edeb80ad1b2d76c3b4fed93a79dd8458cb6c4f
Tim Guite <tim.guite@diamond.ac.uk>
2020-07-07 16:17:19 +0100

    Template added to provide basic PLC information
    Also edited builder to generate these PVs alongside adding necessary lines to the boot script

--- etc/builder.py
+++ etc/builder.py
@@ -2,15 +2,21 @@ from iocbuilder import AutoSubstitution, Substitution, ModuleBase
 from iocbuilder import Device
 from iocbuilder.arginfo import *
 
-class EtherIPInit(Device):
+class EtherIPInit(Substitution, Device):
     """This creates an IP connection to a plc using the ether_ip driver"""
     # Dependencies = ()
     LibFileList = ['ether_ip']
     DbdFileList = ['ether_ip']
     AutoInstantiate = True
+    # Template file with simple PVs collecting PLC information
+    TemplateFile = "plcInfo.template"
+    # Necessary for substitution object
+    Arguments = ["name", "device"]
     
-    def __init__(self, name, ip):
-        self.__super.__init__()
+    def __init__(self, device, name, ip):
+        # Correctly fill in the template with these arguments
+        self.__super.__init__(device=device, name=name)
+        self.device = device
         self.name = name
         self.ip = ip
 
@@ -18,12 +24,12 @@ class EtherIPInit(Device):
         print "# EtherIP Initialisation"
         print "drvEtherIP_init()"
 
-
     def Initialise(self):
         print "# Define EtherIP to PLC connection"
         print "drvEtherIP_define_PLC(\"{0}\", \"{1}\", 0)".format(self.name, self.ip)
         
     ArgInfo = makeArgInfo(__init__,    
+        device = Simple("PV Prefix"),
         name = Simple("Port Name"),
         ip = Simple("IP Port of PLC"))
 
