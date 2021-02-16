082dda98d2971b93d721eca30b58070fa0e63f43
Omar Elamin <omar.elamin@diamond.ac.uk>
2021-02-16 15:59:54 +0000

    tox must be called with --recreate otherwise test will fail once changed.

--- pyether_ip/tests/test_ether_ip.py
+++ pyether_ip/tests/test_ether_ip.py
@@ -1,6 +1,19 @@
+import pytest
+
 from eip_client import Client
 
 def test_int_reading():
     i = Client.get_int('PLC_Interface[23].Num', '172.23.243.77')
     assert isinstance(i, int)
 
+def test_string_reading():
+    s = Client.get_string('V[1].Interface_Desc0', '172.23.243.77')
+    assert isinstance(s, str)
+
+def test_double_reading():
+    d = Client.get_double('V[1].Position', '172.23.243.77')
+    assert isinstance(d, float)
+
+def test_mismatch_type_raises_exception():
+    with pytest.raises(Exception):
+        Client.get_int('V[1].Interface_Desc0', '172.23.243.77') # V[]..Desc0 stores a string
